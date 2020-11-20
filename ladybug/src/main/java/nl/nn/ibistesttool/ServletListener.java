44cb0cc28197d481ec61d03bc818b1d6de528b63
Murat Kaan Meral <muratkaanmeral@gmail.com>
2020-11-20 17:16:07 +0100

    Initialize ladybug servlets

---
+++ ladybug/src/main/java/nl/nn/ibistesttool/ServletListener.java
@@ -0,0 +1,33 @@
+package nl.nn.ibistesttool;
+
+import nl.nn.testtool.web.ApiServlet;
+import nl.nn.testtool.web.FrontendServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.ServletRegistration;
+import javax.servlet.annotation.WebListener;
+
+@WebListener
+public class ServletListener implements ServletContextListener {
+	@Override
+	public void contextInitialized(ServletContextEvent servletContextEvent) {
+		ServletContext context = servletContextEvent.getServletContext();
+		// Add ladybug backend
+		ServletRegistration.Dynamic serv = context.addServlet("ladybug", ApiServlet.class);
+		serv.setLoadOnStartup(0);
+		serv.addMapping("/ladybug/*");
+		serv.setInitParameters(ApiServlet.getInitParameters());
+
+		// Add ladybug frontend server
+		serv = context.addServlet("ladybug-frontend", FrontendServlet.class);
+		serv.setLoadOnStartup(0);
+		serv.addMapping("/ladybug/frontend/*");
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent servletContextEvent) {
+
+	}
+}
